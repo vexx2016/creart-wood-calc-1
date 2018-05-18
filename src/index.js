4c42a2935292a844a743423ee4fde1621cc3d221
Anton Morya <anton.morya@gmail.com>
2018-05-18 13:48:43 +0300

    JSON structure changed

--- src/index.js
+++ src/index.js
@@ -10,11 +10,15 @@ let xhr = new XMLHttpRequest();
 xhr.open('GET', 'calc.json');
 xhr.onload = function () {
     if (xhr.status === 200) {
+        console.log(data);
         let data = JSON.parse(xhr.responseText)['calc'];
-        ReactDOM.render(<App data={data} />, document.getElementById('root'));
+        // ReactDOM.render(<App data={data} />, document.getElementById('root'));
+        ReactDOM.render(<h1>Under reconstruction</h1>, document.getElementById('root'));
+        
     }
     else {
         ReactDOM.render(<h1>Что-то пошло не так. Пожалуйста, свяжитесь с нами</h1>, document.getElementById('root'));
     }
 };
+
 xhr.send(); 
